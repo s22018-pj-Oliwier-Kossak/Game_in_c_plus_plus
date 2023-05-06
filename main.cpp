e9d14bc41601b0d53554a6b3517088f3045542b9
Oliwier Kossak <s22018@pjwstk.edu.pl>
2023-05-06 18:35:30 +0200

    add background to game

--- main.cpp
+++ main.cpp
@@ -1,40 +1,45 @@
 
 #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <iostream>
 
+using namespace std;
 
-bool handle_events(SDL_Rect &rect);
+SDL_Window *window;
+SDL_Surface *background ;
+SDL_Surface *screen_surface;
+
+bool handle_events();
+bool load_background();
 
 int main(int argc, char* argv[]){
+
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("cannon game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,1000,800,SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("cannon game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,1100,800,SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    background = SDL_LoadBMP("background.bmp");
+    screen_surface = SDL_GetWindowSurface( window );
 
-
-
-    SDL_Rect rect;
-    rect.x = 250;
-    rect.y = 150;
-    rect.w = 200;
-    rect.h = 200;
 
     auto prev_ticks = SDL_GetTicks64();
     int frame_dropped = 0;
 
-    while (handle_events(rect)){
+    while (true){
+
         if (!frame_dropped){
 
-            SDL_SetRenderDrawColor(renderer,255,0,0,255);
             SDL_RenderClear(renderer);
-            SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-            SDL_RenderDrawRect(renderer, &rect);
             SDL_RenderPresent(renderer);
+            SDL_BlitSurface( background, NULL, screen_surface, NULL );
+            SDL_UpdateWindowSurface( window );
 
         }
 
         auto ticks = SDL_GetTicks64();
-        if ((ticks - prev_ticks) < 33){
-            SDL_Delay(33 - (ticks - prev_ticks));
+        if ((ticks - prev_ticks) < 2){
+            SDL_Delay(70 - (ticks - prev_ticks));
             frame_dropped = 0;
+            SDL_UpdateWindowSurface( window );
         }
         else{frame_dropped = 1;}
         prev_ticks += 33;
@@ -48,7 +53,15 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-bool handle_events(SDL_Rect &rect){
+/*bool load_background(){
+
+    bool success = true;
+
+	background = SDL_LoadBMP("background.bmp");
+	return success;
+} */
+
+bool handle_events(){
     /*function to catch events*/
   auto *key_state = SDL_GetKeyboardState(nullptr);
   SDL_Event event;
@@ -59,13 +72,13 @@ bool handle_events(SDL_Rect &rect){
 
             }
         }
-    if (key_state[SDL_SCANCODE_UP]){
+   /* if (key_state[SDL_SCANCODE_UP]){
         rect.y -=5;
     }
     if (key_state[SDL_SCANCODE_DOWN]){
         rect.y +=5;
     }
-
+*/
     return true;
 }
 
